3ddb403016c217346bfb707c49fad1c0dc28d82d
Emmanuel M <38224811+EmmanuelDevM@users.noreply.github.com>
2019-03-23 11:25:59 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,105 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const Enmap = require("enmap")
+const fs = require("fs");
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+ 
+const adapter = new FileSync('db.json')
+const db = low(adapter)
+
+db.defaults({ notes_public: [], notes_private: []}).write()
+
+const config = require("./config.json");
+client.config = config;
+
+var now = new Date();
+var hour = now.getHours();
+var minute = now.getMinutes();
+var second = now.getSeconds();
+var times = (`[${hour}:${minute}:${second}]/`);
+
+client.on('ready', () => {
+  console.log(times+`\x1b[33m%s\x1b[0m`,'[WARN]','\x1b[0m','Connexion en cours...');
+  console.log(times+`\x1b[33m%s\x1b[0m`,'[WARN]','\x1b[0m','Connexion à l\'API Discord.js en cours...');
+  console.log(times+`\x1b[32m%s\x1b[0m`,'[OK]','\x1b[0m', 'Connexion à l\'API Discord.js effectuée');
+  console.log(times+`\x1b[36m%s\x1b[0m`,'[INFO]', '\x1b[0m','Connecté sur ' + client.user.username + '#' + client.user.discriminator);
+  console.log(times+`\x1b[32m%s\x1b[0m`,'[OK]','\x1b[0m','Chargement terminé');
+  console.log(times+`\x1b[32m%s\x1b[0m`,'[OK]','\x1b[0m','Prêt et connecté');
+
+  const activities = [
+      "b!help | Delta",
+      `b!help | ${client.guilds.size} serveurs`,
+      `b!help | ${client.users.size} users`
+  ];
+  client.setInterval(() => {
+      const index = Math.floor(Math.random() * activities.length);
+      client.user.setActivity(activities[index], {
+          type: "PLAYING",
+          url: "http://twitch.tv/client"
+      });
+  }, 15000);
+});
+
+
+client.login(config.token);
+
+fs.readdir("./events/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    const event = require(`./events/${file}`);
+    let eventName = file.split(".")[0];
+    client.on(eventName, event.bind(null, client));
+  });
+});
+
+client.commands = new Enmap();
+
+fs.readdir("./commands/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    if (!file.endsWith(".js")) return;
+    let props = require(`./commands/${file}`);
+    let commandName = file.split(".")[0];
+    client.commands.set(commandName, props);
+  });
+});
+
+client.commands = new Enmap();
+
+fs.readdir("./commands/utils/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    if (!file.endsWith(".js")) return;
+    let props = require(`./commands/utils/${file}`);
+    let commandName = file.split(".")[0];
+    client.commands.set(commandName, props);
+  });
+});
+
+client.commands = new Enmap();
+
+fs.readdir("./commands/notes_public/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    if (!file.endsWith(".js")) return;
+    let props = require(`./commands/notes_public/${file}`);
+    let commandName = file.split(".")[0];
+    client.commands.set(commandName, props);
+  });
+});
+
+client.commands = new Enmap();
+
+fs.readdir("./commands/notes_private/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    if (!file.endsWith(".js")) return;
+    let props = require(`./commands/notes_private/${file}`);
+    let commandName = file.split(".")[0];
+    client.commands.set(commandName, props);
+  });
+});
+
+
+
